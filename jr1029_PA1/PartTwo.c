a4537eeb27ac35172f87de76cb4e0a937e30f6b2
Jimena Romo Cuevas <romojimena244@gmail.com>
2022-09-22 20:55:34 -0500

    moved factorial function for compiling

--- jr1029_PA1/PartTwo.c
+++ jr1029_PA1/PartTwo.c
@@ -1,6 +1,16 @@
 //Jimena Romo Cuevas -CSCE 2610 UNT
 #include <stdio.h>
 
+//creating a function for factorials
+int factorial(int n)
+{
+    int z = 1;
+    for(int i = 1; i <= n; i ++)
+    {
+        z = z * i;
+    }
+    return z;
+}
 
 //main
 int main()
@@ -16,7 +26,7 @@ int main()
     //calculating value of y
     for(int i =0; i<= a; i++)
     {
-        y += ((factorial(i*2)) + b);
+        y += (factorial(i*2) + b);
     }
 
     printf("\ny = %d",y); //printing y
@@ -24,13 +34,3 @@ int main()
     return 0;
 }
 
-//creating a function for factorials
-int factorial(int n)
-{
-    int z = 1;
-    for(int i = 1; i <= n; i ++)
-    {
-        z = z * i;
-    }
-    return z;
-}
