31fff91e4b64b8ea09dbfe7b2ca97394032e71a5
Jimena Romo Cuevas <romojimena244@gmail.com>
2022-09-22 20:43:30 -0500

    calculating y

--- jr1029_PA1/PartTwo.c
+++ jr1029_PA1/PartTwo.c
@@ -5,12 +5,20 @@
 //main
 int main()
 {
-    int a,b;
+    int a,b,y;
+    y = 0;
 
     printf("Enter a value for a: ");
     scanf("%d",&a); //scanning for a
     printf("\nEnter a value for b: ");
     scanf("%d",&b); //scanning into b;
+
+    //calculating value of y
+    for(int i =0; i<= a; i++)
+    {
+        y += ((factorial(n*2)) + b);
+    }
+
 
     return 0;
 }
