8537be0e3b5826e5dfebdeba5baf0426a7e0d54a
Jimena Romo Cuevas <romojimena244@gmail.com>
2022-09-22 21:52:17 -0500

    declaring

---
+++ jr1029_PA1/PartThree.c
@@ -0,0 +1,17 @@
+//c program finding number of words/vowels.and length
+//Jimena Romo Cuevas - CSCE 2610
+#include <stdio.h>
+
+int main()
+{
+    char sentence[256]; //character array of no more than 255 characters - last is null
+    int vowels, consonant, digit, space;
+    vowels = 0;
+    consonant = 0;
+    space = 0;
+    digit = 0;
+
+    //geting line of string input
+
+    return 0;
+}
