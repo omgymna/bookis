058215ecaaf2a171edbd3989045ca8c36c811c3d
Jimena Romo Cuevas <romojimena244@gmail.com>
2022-09-22 20:18:09 -0500

    adding b in the function and addition

--- jr1029_PA1/PartOne.c
+++ jr1029_PA1/PartOne.c
@@ -4,11 +4,14 @@
 
 int main()
 {
-    int a,n,y,i,x;
+    int a,b,n,y,i,x;
     y = 0;
 
     printf("Enter a value for a: ");
     scanf("%d",&a); //scanning a from user
+
+    printf("Enter a value for b: ");
+    scanf("%d",&b); //scanning b from user
 
     for(n=1;n<=a;n++)
     {
@@ -16,13 +19,13 @@ int main()
 
         for(i=1;i<=n;i++)
         {
-            p = n*p; //n^n without power function
+            x = n*x; //n^n without power function
         }
 
-        y = y+x+a;  //summing and storing result inside of y
+        y = y+x+b;  //summing and storing result inside of y
     }
 
-    printf("\ny = %d\n",y) //printing y
+    printf("\ny = %d\n",y); //printing y
 
     return 0;
 }
