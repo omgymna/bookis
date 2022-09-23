2e02f9ad025527196efdbb2559daa9cfc742b6d2
Jimena Romo Cuevas <romojimena244@gmail.com>
2022-09-22 22:45:54 -0500

    input of x

---
+++ jr1029_PA1/PartFour.c
@@ -0,0 +1,13 @@
+//computing and printing value for e^(x^2)
+//Jimena Romo Cuevas - CSCE 2610
+#include <stdio.h>
+int main()
+{
+    float x;
+    int a;
+
+    printf("Enter value of x: ");
+    scanf("%f",&x); //scanning floating point variable to x
+
+    return 0;
+}
