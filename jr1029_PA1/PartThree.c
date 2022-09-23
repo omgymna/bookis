20898233933032cd8c64e74654caa3876bc5a59d
Jimena Romo Cuevas <romojimena244@gmail.com>
2022-09-22 22:21:47 -0500

    done with if statements, working on dets of length

--- jr1029_PA1/PartThree.c
+++ jr1029_PA1/PartThree.c
@@ -4,18 +4,33 @@
 
 int main()
 {
-    char sentence[256]; //character array of no more than 255 characters - last is null
-    int vowels, consonant, digit, space;
+    char sentence[255]; //character array of no more than 255 characters 
+    int vowels, length, words;
     vowels = 0;
-    consonant = 0;
-    space = 0;
-    digit = 0;
+    length = 0;
+    words = 1;
 
     //geting line of string input
     printf("Enter sentence: ");
     fgets(sentence,sizeof(sentence), stdin);
 
-    
+    while(sentence[length]!='\0')
+    {
+        if(sentence[length]==' ')
+        {
+            words++;
+        }
+        if(sentence[length]=='a'||sentence[length]=='e'||sentence[length]=='i'||sentence[length]=='o'||sentence[length]=='u'||sentence[length]=='A'||sentence[length]=='E'||sentence[length]=='I'||sentence[length]=='O'||sentence[length]=='U')
+        {
+            vowels++;
+        }
+
+        length++;
+    }
+
+    printf("Number of words: %d", words);
+    printf("\nNumber of vowels: %d", vowels);
+    printf("\nLength of sentence: %d\n", length);
 
     return 0;
 }
