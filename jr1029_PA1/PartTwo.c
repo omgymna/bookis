ab2d333618cf482c7c22aae745cd6239d21077b2
Jimena Romo Cuevas <romojimena244@gmail.com>
2022-09-22 20:39:17 -0500

    printing and scanning for values

--- jr1029_PA1/PartTwo.c
+++ jr1029_PA1/PartTwo.c
@@ -1,6 +1,20 @@
 //Jimena Romo Cuevas -CSCE 2610 UNT
 #include <stdio.h>
 
+
+//main
+int main()
+{
+    int a,b;
+
+    printf("Enter a value for a: ");
+    scanf("%d",&a); //scanning for a
+    printf("\nEnter a value for b: ");
+    scanf("%d",&b); //scanning into b;
+
+    return 0;
+}
+
 //creating a function for factorials
 int factorial(int n)
 {
