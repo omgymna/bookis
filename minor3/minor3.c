1d8efbde98d7959f5dd0aac81d613b88d94861e7
Jimena Romo Cuevas <romojimena244@gmail.com>
2022-11-04 18:22:45 -0500

    adding conditional

--- minor3/minor3.c
+++ minor3/minor3.c
@@ -6,9 +6,15 @@
 // shared variables
 char shared_buffer[NITEMS]; // echo buffer
 int shared_count; // item count
+
 pthread_mutex_t mutex; // pthread mutex
 unsigned int prod_index = 0; // producer index into shared buffer
 unsigned int cons_index = 0; // consumer index into shard buffer
+
+//added:
+pthread_cond_t cond_full;   //when shared buffer is full
+pthread_cond_t cond_empty;  //when its empty
+
 // function prototypes
 void * producer(void *arg);
 void * consumer(void *arg);
@@ -16,19 +22,29 @@ void * consumer(void *arg);
 int main()
 {
     pthread_t prod_tid, cons_tid1, cons_tid2;
+    
     // initialize pthread variables
     pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cond_full, NULL);
+    pthread_cond_init(&cond_empty, NULL);
+    
     // start producer thread
     pthread_create(&prod_tid, NULL, producer, NULL);
+
     // start consumer threads
     pthread_create(&cons_tid1, NULL, consumer, NULL);
     pthread_create(&cons_tid2, NULL, consumer, NULL);
+
     // wait for threads to finish
     pthread_join(prod_tid, NULL);
     pthread_join(cons_tid1, NULL);
     pthread_join(cons_tid2, NULL);
+
     // clean up
     pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond_full);
+    pthread_cond_destroy(&cond_empty);
+
     return 0;
 }
 
@@ -37,11 +53,14 @@ void * producer(void *arg)
 {
     char key;
     printf("Enter text for producer to read and consumer to print, use Ctrl-C to exit.\n\n");
+    
     // this loop has the producer read data in from stdin and place the read data on the shared buffer
     while (1)
     {
         // read input key
         scanf("%c", &key);
+
+        
 
         // this loop is used to poll the shared buffer to see if it is full:
         // -- if full, unlock and loop again to keep polling
