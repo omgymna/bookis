a15809fc287ae63d395ba63205bbda41f5539d29
omgymna <romojimena244@gmail.com>
2023-10-14 22:48:30 -0500

    Update MinimumMult.cpp

--- Algorithms/MinimumMult.cpp
+++ Algorithms/MinimumMult.cpp
@@ -12,37 +12,42 @@ using namespace std;
 
 int MinimumMultiplication(int a[], int n)
 {
-    int m[n][n];    //used to store the costs (like in the table)
-    int i,j;
-    int k, l, q;
 
     //diagonals are 0;
-    for(i = 1; i < n; i++)
-    {
+    int m[n][n];            //used to store the costs (like in the table)
+ 
+    int i, j;
+    int k, L, q;
+ 
+    // cost is zero when multiplying one matrix.
+    for (i = 1; i < n; i++)
         m[i][i] = 0;
-    }
-
-    for(l = 2; l < n; l++)
+ 
+    // chain length (number of matrices being multiplied)
+    for (L = 2; L < n; L++) 
     {
-        for( i = 1; i < n - l; i++)
+        for (i = 1; i < n - L + 1; i++) 
         {
-            j = i + l - 1;
-            m[i][j] = INT_MAX; //set as max integer
-            for(k=i; k<=j-1 ; k++)  //k values during multiplication
+            j = i + L - 1;
+            m[i][j] = INT_MAX;
+            for (k = i; k <= j - 1; k++) //different values of k
             {
-                q = m[i][k] + m[k + 1][j] + a[i - 1] * a[k] * a[j];  //formula -> q is the cost at value k
+                // q = cost of the multiplications
+                q = m[i][k] + m[k + 1][j] + a[i - 1] * a[k] * a[j];
                 if (q < m[i][j])
-                {
                     m[i][j] = q;
-                }
             }
         }
     }
-
-    return m[n][n-1];
+ 
+    return m[1][n - 1];
 }
 
 int main()
 {
+    int arr[] = {1,2,3,4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout<< "minimum number of multiplications is "<< MinimumMultiplication(arr, size)<<endl;
     return 0;
 }
